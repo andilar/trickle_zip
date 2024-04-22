9df4258b80e2e0a757d176ca91972b12ae5434c9
andilar <andilar@gmail.com>
2024-04-22 10:26:38 +0200

    cleanup

--- tricklezip/target/debug/deps/tricklezip-9100e7fe68a1900b.d
+++
@@ -1,5 +0,0 @@
-C:\Users\I009847\Documents\code\tricklezip\tricklezip\target\debug\deps\libtricklezip-9100e7fe68a1900b.rmeta: src\lib.rs
-
-C:\Users\I009847\Documents\code\tricklezip\tricklezip\target\debug\deps\tricklezip-9100e7fe68a1900b.d: src\lib.rs
-
-src\lib.rs:
