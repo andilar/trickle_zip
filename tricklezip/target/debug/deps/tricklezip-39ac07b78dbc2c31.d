e3456f465a085e3325ad86c69d821be9fe252902
andilar <andilar@gmail.com>
2024-04-22 10:22:44 +0200

    Added basic lib environemnt

---
+++ tricklezip/target/debug/deps/tricklezip-39ac07b78dbc2c31.d
@@ -0,0 +1,5 @@
+C:\Users\I009847\Documents\code\tricklezip\tricklezip\target\debug\deps\libtricklezip-39ac07b78dbc2c31.rmeta: src\lib.rs
+
+C:\Users\I009847\Documents\code\tricklezip\tricklezip\target\debug\deps\tricklezip-39ac07b78dbc2c31.d: src\lib.rs
+
+src\lib.rs:
---
+++ tricklezip/target/debug/deps/tricklezip-9100e7fe68a1900b.d
@@ -0,0 +1,5 @@
+C:\Users\I009847\Documents\code\tricklezip\tricklezip\target\debug\deps\libtricklezip-9100e7fe68a1900b.rmeta: src\lib.rs
+
+C:\Users\I009847\Documents\code\tricklezip\tricklezip\target\debug\deps\tricklezip-9100e7fe68a1900b.d: src\lib.rs
+
+src\lib.rs:
